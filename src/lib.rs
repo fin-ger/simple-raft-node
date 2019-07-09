1688f0b1b7946d87938038516466e3ed9852185d
Fin Christensen <fin-ger@users.noreply.github.com>
2019-07-09 15:23:09 +0200

    Update comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
-// Copyright 2019 PingCAP, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// See the License for the specific language governing permissions and
-// limitations under the License.
 #![feature(
     vec_remove_item,
     drain_filter,
@@ -32,7 +20,7 @@
  *
  * Why no API documentation?
  *
- * This is a prototype and neither the API nor the functionality are
+ * This is a prototype and the API and the functionality are
  * far from stable. This work is meant to explore possibilities!
  * As long as not otherwise stated, this work will remain undesirable
  * for production purposes!
